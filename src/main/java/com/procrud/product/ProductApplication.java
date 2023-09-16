564588c0bc5eee3d0746bb2260b6c294f21d76ef
Andre Luiz <andregmdias@gmail.com>
2023-09-16 20:08:57 -0300

    initial commit

---
+++ src/main/java/com/procrud/product/ProductApplication.java
@@ -0,0 +1,13 @@
+package com.procrud.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+	}
+
+}
